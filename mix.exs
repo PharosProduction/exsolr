1383006ffa9a08a961ef3423dec5665b2ae0f245
Dmytro Nasyrov <dmytro@pharosproduction.com>
2020-02-28 22:51:03 +0200

    update httpoison version

--- mix.exs
+++ mix.exs
@@ -48,8 +48,8 @@ defmodule Exsolr.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:httpoison, "~> 0.8.0"},
-      {:poison, "~> 2.0"},
+      {:httpoison, "~> 1.6"},
+      {:poison, "~> 4.0"},
       {:credo, "~> 0.3", only: [:dev, :test]},
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc, "~> 0.11", only: :dev},
